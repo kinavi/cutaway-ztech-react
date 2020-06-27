fd1782b6dd058e91af7afc0776265907d91dba80
kinavi <wolfmariev@gmail.com>
2020-06-27 10:18:19 +0500

    fix conflict merge

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import { Provider } from 'react-redux';
-import App from './App';
-=======
 import {
   BrowserRouter,
 } from 'react-router-dom';
@@ -12,7 +8,6 @@ import App from './container/App';
 import './SuperFormReset.css';
 import 'normalize.css';
 import './style/index.sass';
->>>>>>> fb539230b08e5923944bb881e0bcf77a3b063974
 
 const Client = () => (
   <BrowserRouter>
